829eba4cf26cd0e71dbd8263f98a2fe956b92b31
yular <1062969706@qq.com>
2014-08-23 19:20:14 -0700

    Log in/Sign in Panel

--- LifeStatistics/src/com/xcc/mainrun/Main.java
+++ LifeStatistics/src/com/xcc/mainrun/Main.java
@@ -7,10 +7,16 @@ import java.awt.*;
 import javax.sql.*;
 import java.text.*;
 
+import com.xcc.ui.*;
+
 public class Main {
 	
 	public static void main(String[] args){
-		
+		LogIn login = new LogIn();
+		login.setSize(500, 430);
+		login.setVisible(true);
+		login.setLocationRelativeTo(null);
+		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
---
+++ LifeStatistics/src/com/xcc/ui/LogIn.java
@@ -0,0 +1,47 @@
+package com.xcc.ui;
+
+import javax.swing.*;
+import javax.sql.*;
+
+import java.awt.*;
+
+public class LogIn extends JFrame{
+	
+	private JTextArea userName = new JTextArea();
+	private JTextArea passWord = new JTextArea();
+	private JLabel username = new JLabel("User Name: ");
+	private JLabel password = new JLabel("PassWord: ");
+	private JButton logIn = new JButton("Log In");
+	private JButton sigIn = new JButton("Sign In");
+	private ImageIcon companyIcon = new ImageIcon("images/lsicon.png");
+	private JPanel mainPanel = new JPanel();
+	private Box boxIcon = Box.createHorizontalBox();
+	private Box boxUser = Box.createHorizontalBox();
+	private Box boxPW = Box.createHorizontalBox();
+	private Box boxBtn = Box.createHorizontalBox();
+	
+	public LogIn(){
+		boxIcon.add(new JLabel(companyIcon));
+		boxUser.add(username);
+		boxUser.add(Box.createHorizontalStrut(20));
+		boxUser.add(userName);
+		boxPW.add(password);
+		boxPW.add(Box.createHorizontalStrut(25));
+		boxPW.add(passWord);
+		boxBtn.add(logIn);
+		boxBtn.add(Box.createHorizontalStrut(100));
+		boxBtn.add(sigIn);
+		
+		setLayout(null);
+		
+		add(boxIcon);
+		add(boxUser);
+		add(boxPW);
+		add(boxBtn);
+		
+		boxIcon.setBounds(70, 20, 500, 180);
+		boxUser.setBounds(50, 260, 320, 20);
+		boxPW.setBounds(50, 300, 320, 20);
+		boxBtn.setBounds(100, 330, 500, 50);
+	}
+}
